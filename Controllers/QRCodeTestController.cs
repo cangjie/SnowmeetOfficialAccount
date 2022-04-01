7cde26e8b551df98fcdc63fbd7c0302a8ea6edd3
cangjie <cangjie@hotmail.com>
2022-04-01 09:25:33 +0800

    start qrcode

---
+++ Controllers/QRCodeTestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using ThoughtWorks.QRCode.Codec;
+using System.Drawing;
+using System.IO;
+
+namespace LuqinOfficialAccount.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class QRCodeTestController : ControllerBase
+    {
+        
+        public QRCodeTestController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<string> GetQrCodeUrl(string qrCodeString)
+        {
+            QRCodeEncoder enc = new QRCodeEncoder();
+            Bitmap bmp = enc.Encode(qrCodeString);
+
+            return "";
+        }
+
+
+    }
+}
