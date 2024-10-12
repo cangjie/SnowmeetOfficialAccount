87fc1c5287f930978205f8e2c98df0104abb19b7
苍杰 <cangjie@hotmail.com>
2024-10-12 20:28:01 +0800

    unknowm

---
+++ Models/OAUserInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace SnowmeetOfficialAccount.Models
+{
+	[Table("oa_user_info")]
+	public class OAUserInfo
+	{
+		[Key]
+		public string openid { get; set; }
+
+		public int subscribe { get; set; }
+		public string language { get; set;}
+		public long subscribe_time { get; set; }
+		public string unionid { get; set; }
+		public string remark { get; set; }
+		public int groupid { get; set; }
+		public string? tagid_list_str { get; set; }
+		public long qr_scene { get; set; }
+		public string qr_scene_str { get; set; }
+
+        [NotMapped]
+        public int[] tagid_list  { get; set; }
+    }
+}
+
